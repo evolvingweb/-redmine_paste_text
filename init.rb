45b5deeab322b11290c99b9038aa39b3fa9c08d3
Burak Ikiler <123000591+burak-ikiler-ew@users.noreply.github.com>
2024-05-22 12:33:56 +0300

    Update init.rb for version upgrade

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_paste_text do
   url 'https://github.com/evolvingweb/redmine_paste_text'
   author_url 'http://evolvingweb.ca'
   description 'Contains paste extension inspired by https://puppypaste.com/'
-  version '0.0.1'
+  version '0.0.2'
   requires_redmine :version_or_higher => '3.4'
 
 end
